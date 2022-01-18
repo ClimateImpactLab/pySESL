f7175095bda7b86aaf1be606f7fc3fa26742be45
Ian Bolliger <bolliger32@gmail.com>
2022-01-17 20:34:57 -0800

    Sep rebase and ics (#4)
    * separate the resampling of ICs and rebasing of T funcs
    
    * autofmt

--- pySESL/project.py
+++ pySESL/project.py
@@ -133,7 +133,7 @@ def resample_ics(ics, sim_ids, sesl_trained_params):
     out_params = xr.merge((T0_2000, c_2000, T_ref, param_sims))
     return out_params
 
-
+  
 def bias_correct_temps(temps, bc_period, T_ref, first_year=None):
     """Resample ICs such that they have the same number of samples as are in the
     temperature projections ``temps``.
@@ -152,7 +152,6 @@ def project_sesl(temps, params):
 
     TODO: finish docstring
     """
-
     temp_arr = temps.copy()
     sl = 0 * temp_arr
 
